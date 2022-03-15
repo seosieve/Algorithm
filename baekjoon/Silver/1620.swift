bf1341eba7e8551e7f6e7b9c8acb548e643d8b98
seosieve <royalcircle97@naver.com>
2022-03-15 23:49:47 +0900

    Daily Solve

---
+++ baekjoon/Silver/1620.swift
@@ -0,0 +1,21 @@
+//1620 나는야 포켓몬 마스터 이다솜
+import Foundation
+
+let NM = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (N,M) = (NM[0], NM[1])
+var book1 = [String : Int]()
+var book2 = Array(repeating: "", count: N)
+for index in 0..<N {
+    let pokemon = readLine()!
+    book1[pokemon] = index+1
+    book2[index] = pokemon
+}
+
+for _ in 0..<M {
+    let input = readLine()!
+    if let num = Int(input) {
+        print(book2[num-1])
+    } else {
+        print(book1[input]!)
+    }
+}
