a3b053e162a051551e002142e5484304130be70e
seosieve <royalcircle97@naver.com>
2022-08-15 15:26:56 +0900

    ADD: Daily Solve

--- baekjoon/Silver/1012.swift
+++ baekjoon/Silver/1012.swift
@@ -1,6 +1,5 @@
 //1012 유기농 배추
-import Foundation
-
+//1
 func movingCabbage(_ field: inout [[Int]], _ x: Int, _ y: Int, _ maxX: Int, _ maxY: Int) {
     field[x][y] = 0
     if x-1 >= 0 {
@@ -45,3 +44,39 @@ for _ in 0..<Int(readLine()!)! {
     }
     print(earthwormCount)
 }
+
+//2 BFS
+for _ in 0..<Int(readLine()!)! {
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    let (M, N, k) = (input[0], input[1], input[2])
+    var map = Array(repeating: Array(repeating: 0, count: N), count: M)
+    for _ in 0..<k {
+        let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+        map[input[0]][input[1]] = 1
+    }
+
+    var queue = [(Int, Int)]()
+    var count = 0
+
+    for row in 0..<M {
+        for col in 0..<N {
+            if map[row][col] == 1 {
+                queue.append((row, col))
+                map[row][col] = 0
+                while !queue.isEmpty {
+                    let node = queue.removeFirst()
+                    for i in [(-1, 0),(1, 0),(0, -1),(0, 1)] {
+                        let (m, n) = (node.0+i.0, node.1+i.1)
+                        if (0..<M).contains(m) && (0..<N).contains(n) && map[m][n] == 1 {
+                            queue.append((m, n))
+                            map[m][n] = 0
+                        }
+                    }
+                }
+                count += 1
+            }
+        }
+    }
+    print(count)
+}
+
