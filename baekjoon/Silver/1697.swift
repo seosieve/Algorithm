f20dec2a9ea8d3ed70d81b1993665208a0ac4825
seosieve <royalcircle97@naver.com>
2022-08-16 15:49:50 +0900

    Daily Solce

--- baekjoon/Silver/1697.swift
+++ baekjoon/Silver/1697.swift
@@ -1,21 +1,52 @@
 import Foundation
 
 //1697 숨바꼭질
-let NM = readLine()!.split(separator: " ").map{Int(String($0))!}
-let (N,M) = (NM[0], NM[1])
-var book1 = [String : Int]()
-var book2 = Array(repeating: "", count: N)
-for index in 0..<N {
-    let pokemon = readLine()!
-    book1[pokemon] = index+1
-    book2[index] = pokemon
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (N,K) = (input[0],input[1])
+
+var queue = [(0,N)]
+var index = 0
+var visited = Array(repeating: false, count: 100001)
+visited[N] = true
+
+while true {
+    let node = queue[index]
+    if node.1 == K {
+        print(node.0)
+        break
+    }
+    for i in [node.1+1, node.1-1, node.1*2] {
+        if i <= 100000 && i >= 0 && !visited[i] {
+            queue.append((node.0+1, i))
+            visited[i] = true
+        }
+    }
+    index += 1
 }
 
-for _ in 0..<M {
-    let input = readLine()!
-    if let num = Int(input) {
-        print(book2[num-1])
-    } else {
-        print(book1[input]!)
+//1697 숨바꼭질
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (N, K) = (input[0], input[1])
+
+var queue = [(0, N)]
+var count = 0
+var visited = Array(repeating: false, count: 100001)
+visited[N] = true
+
+func BFS() {
+    let node = queue[count]
+
+    if node.1 == K {
+        print(node.0)
+        return
     }
+    for i in [node.1-1, node.1+1, node.1*2] {
+        if i >= 0 && i <= 100000 && !visited[i] {
+            queue.append((node.0+1, i))
+            visited[i] = true
+        }
+    }
+    count += 1
+    BFS()
 }
+BFS()
