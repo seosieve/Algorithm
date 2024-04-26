2487144cafdc80ebba030d62d33898cc858a853d
seosieve <royalcircle97@naver.com>
2024-04-26 23:40:43 +0900

    Daily Solve

---
+++ baekjoon/Greedy/2170 선 긋기.swift
@@ -0,0 +1,97 @@
+//
+//  Algorithms
+//
+//  Created by 서충원
+//
+
+import Foundation
+
+//2170 선 긋기
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+        // 인덱스 범위 넘어가는 것 방지
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let file = FileIO()
+let N = file.readInt()
+var lineArr = [(Int, Int)]()
+
+for _ in 0..<N {
+    let start = file.readInt(), end = file.readInt()
+    lineArr.append((start, end))
+}
+
+lineArr.sort{ $0.0 < $1.0 }
+
+var start = lineArr[0].0
+var end = lineArr[0].1
+var result = 0
+
+for i in 1..<N {
+    if end >= lineArr[i].0 {
+        if end < lineArr[i].1 {
+            end = lineArr[i].1
+        }
+    } else {
+        result += end - start
+        start = lineArr[i].0
+        end = lineArr[i].1
+    }
+}
+
+result += end - start
+
+print(result)
